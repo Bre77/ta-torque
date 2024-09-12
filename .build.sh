a9f1696fc8858d49fead5dee8c31a962151f561a
Brett <brett.whynot@gmail.com>
2024-09-12 11:33:13 +1000

    Fix build

--- .build.sh
+++ .build.sh
@@ -3,6 +3,6 @@ cd "${0%/*}"
 OUTPUT="${1:-ta-torque.spl}"
 chmod -R u=rwX,go= *
 chmod -R u-x+X *
-chmod -R u=rwx,go= *
+chmod -R u=rwx,go= bin/*
 cd ..
 tar -cpzf $OUTPUT --exclude=.* --exclude=package.json --overwrite ta-torque
